777dc99f9cae2d22aee61801126eff227d741ada
Shah rahman <toshah.rahman@gmail.com>
2022-05-22 09:42:26 +0300

    fastapi fun v4 3

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from fastapi import FastAPI
 import uvicorn
+from enum import Enum
 
 app = FastAPI()
+
+
+class Department(str, Enum):
+    IT = 'IT'
+    Prorgrammin = 'Programming'
+    Database = 'Database'
 
 
 @app.get('/status')
 def check_status():
     return f'Hello World!'
+
+
+@app.get('students/{student_id}')
+def check_students(student_id: int, department: Department, gender: str = None):
+    return f'list of students'
 
 
 if __name__ == '__main__':
