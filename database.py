0cead5a66dda68a6538dd9fbdb12022d158908dd
Shah rahman <toshah.rahman@gmail.com>
2022-05-23 07:09:54 +0300

    fastapi fun v4 9

---
+++ database.py
@@ -0,0 +1,11 @@
+import sqlalchemy as sqlalchemy
+import sqlalchemy.ext.declarative as declarative
+import sqlalchemy.orm as orm
+
+DB_URL = "sqlite:///./test.db"
+
+engine = sqlalchemy.create_engine(DB_URL, connect_args={"check_same_thread"})
+
+SessionLocal = orm.sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+Base = declarative.declarative_base()
