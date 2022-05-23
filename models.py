64c67660f4d7db6d8e890bf794f911d1c9044560
Shah rahman <toshah.rahman@gmail.com>
2022-05-23 17:39:41 +0300

    fastapi fun v4 13

--- models.py
+++ models.py
@@ -13,3 +13,12 @@ class UserModel(_database.Base):
     phone = _sqlalchemy.Column(_sqlalchemy.String)
     password_hash = _sqlalchemy.Column(_sqlalchemy.String)
     created_at = _sqlalchemy.Column(_sqlalchemy.DateTime, default=_datetime.datetime.utcnow())
+
+class PostModel(_database.Base):
+    __tablename__ = "posts"
+    id = _sqlalchemy.Column(_sqlalchemy.Integer, primary_key=True, index=True)
+    user_id = _sqlalchemy.Column(_sqlalchemy.Integer, _sqlalchemy.ForeignKey("users.id"))
+    title = _sqlalchemy.Column(_sqlalchemy.String)
+    content = _sqlalchemy.Column(_sqlalchemy.String)
+    created_at = _sqlalchemy.Column(_sqlalchemy.DateTime, default=_datetime.datetime.utcnow())
+
