67df5b46c3bec87f2c5c22295d5cb043751e2694
Shah rahman <toshah.rahman@gmail.com>
2022-05-23 20:10:59 +0300

    fastapi fun v4 18

--- schemas.py
+++ schemas.py
@@ -1,3 +1,23 @@
-from enum import Enum
 from pydantic import BaseModel
-from typing import Optional
+import datetime as _datetime
+
+
+class UserBase(BaseModel):
+    email: str
+    name: str
+    phone: str
+
+
+class UserRequest(UserBase):
+    password: str
+
+    class Config:
+        orm_mode = True
+
+
+class UserResponse(UserBase):
+    id: int
+    created_at: _datetime.datetime
+
+    class Config:
+        orm_mode = True
