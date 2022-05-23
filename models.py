4b7986dcdd6d7ca2e87582010255372815248637
Shah rahman <toshah.rahman@gmail.com>
2022-05-23 17:29:14 +0300

    fastapi fun v4 11

--- models.py
+++ models.py
@@ -3,3 +3,13 @@ import sqlalchemy as sqlalchemy
 import sqlalchemy.orm as orm
 import passlib.hash as hash
 import database as database
+
+
+class UserModel(database.Base):
+    __tablename__ = "users"
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True, index=True)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    phone = sqlalchemy.Column(sqlalchemy.String)
+    password_hash = sqlalchemy.Column(sqlalchemy.String)
+    created_at = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.utcnow())
