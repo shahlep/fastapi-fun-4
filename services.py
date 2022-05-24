cba1c66050b17bc228d312956a8f2ec7b5d68666
Shah rahman <toshah.rahman@gmail.com>
2022-05-24 19:45:18 +0300

    fastapi fun v4 28

--- services.py
+++ services.py
@@ -4,6 +4,7 @@ import database as _database
 import models as _models
 import schemas as _schemas
 import sqlalchemy.orm as _orm
+import passlib.hash as _hash
 
 
 def create_db():
@@ -33,3 +34,17 @@ async def create_user(user: _schemas.UserRequest, db: _orm.Session):
     except _email_validator.EmailNotValidError:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail="email is not in correct format")
+
+    # convert password to hashed password
+    hashed_password = _hash.bcrypt.hash(user.password)
+
+    user_obj = _models.UserModel(
+        email = email,
+        name = user.name,
+        phone = user.phone,
+        password_hash = hashed_password
+        )
+    db.add(user_obj)
+    db.commit()
+    db.refresh(user_obj)
+    return user_obj
