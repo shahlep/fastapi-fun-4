5736e3aaa69a1cf70866f4a5bae182eca45c4acb
Shah rahman <toshah.rahman@gmail.com>
2022-05-24 16:56:02 +0300

    fastapi fun v4 24

--- services.py
+++ services.py
@@ -1,12 +1,13 @@
 import database as _database
 import models as _models
+import sqlalchemy.orm as _orm
 
 
 def create_db():
     return _database.Base.metadata.create_all(bind=_database.engine)
 
 
-#create_db()
+# create_db()
 
 
 def get_db():
@@ -15,3 +16,7 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def get_user_by_email(email: str, db: _orm.Session):
+    return db.query(_models.UserModel).filter(_models.UserModel.email == email).first()
