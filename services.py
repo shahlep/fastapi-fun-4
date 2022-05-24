08a3ffb1ef9215912e5751ebc5b8a6e66e67f42b
Shah rahman <toshah.rahman@gmail.com>
2022-05-24 18:12:31 +0300

    fastapi fun v4 27

--- services.py
+++ services.py
@@ -1,5 +1,8 @@
+import email_validator as _email_validator
+from fastapi import HTTPException, status
 import database as _database
 import models as _models
+import schemas as _schemas
 import sqlalchemy.orm as _orm
 
 
@@ -20,3 +23,13 @@ def get_db():
 
 async def get_user_by_email(email: str, db: _orm.Session):
     return db.query(_models.UserModel).filter(_models.UserModel.email == email).first()
+
+
+async def create_user(user: _schemas.UserRequest, db: _orm.Session):
+    # check email format
+    try:
+        isValid = _email_validator.validate_email(email=user.email)
+        email = isValid.email
+    except _email_validator.EmailNotValidError:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="email is not in correct format")
