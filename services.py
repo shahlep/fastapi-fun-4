88c73bee2d154b4785fae5ad272ba903f1bf1dcd
Shah rahman <toshah.rahman@gmail.com>
2022-05-24 16:39:24 +0300

    fastapi fun v4 22

--- services.py
+++ services.py
@@ -1,11 +1,13 @@
 import database as _database
+import models as _models
 
 
 def create_db():
     return _database.Base.metadata.create_all(bind=_database.engine)
 
 
-# create_db()
+#create_db()
+
 
 def get_db():
     db = _database.SessionLocal()
