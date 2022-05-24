6c25f8731f9b0af234904efcfd1583885aa73f11
Shah rahman <toshah.rahman@gmail.com>
2022-05-24 16:21:54 +0300

    fastapi fun v4 21

--- services.py
+++ services.py
@@ -5,4 +5,11 @@ def create_db():
     return _database.Base.metadata.create_all(bind=_database.engine)
 
 
-create_db()
+# create_db()
+
+def get_db():
+    db = _database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
