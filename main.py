77b1d21d8e00409534f9915a9f50a5482ef324f4
Shah rahman <toshah.rahman@gmail.com>
2022-05-24 16:48:13 +0300

    fastapi fun v4 23

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import uvicorn
-
+import sqlalchemy.orm as _orm
+import schemas as _schemas
+import services as _services
 
 app = FastAPI()
+
+
+@app.post('/users')
+def register_user(user: _schemas.UserRequest,
+                  db: _orm.Session = Depends(_services.get_db())):
+    pass
 
 
 if __name__ == "__main__":
